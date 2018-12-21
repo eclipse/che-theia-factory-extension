0dcd905a3580ea11d75d3d54b6741e07f0742e43
Vitaliy Guliy <vgulyy@redhat.com>
2018-12-21 15:56:33 +0000

    Cleanup

--- src/factory-theia-client.ts
+++ src/factory-theia-client.ts
@@ -7,8 +7,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
-// import { TheiaCloneCommand, TheiaCommand } from './theia-commands';
-// import { CheFactoryApi, CheFactory } from './che-api';
 import * as theia from '@theia/plugin';
 import * as che from '@eclipse-che/plugin';
 import { TheiaCloneCommand, TheiaCommand } from './theia-commands';
